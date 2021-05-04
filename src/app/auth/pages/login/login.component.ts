210e7ae9c04db46a6c2c993a4916d0641130df52
cabrerojas <gcabrerojas@gmail.com>
2021-05-04 17:19:23 -0400

    ReactiveForms
    Se implementan los formularios reactivos al login y al registro

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  formLogin: FormGroup = this.fb.group({
+    correo: ['test1@test.cl', [ Validators.required, Validators.email ] ],
+    contrasena: ['123456', [ Validators.required, Validators.minLength(6) ] ],
+  });
 
-  ngOnInit(): void {
+  constructor( private fb: FormBuilder) { }
+
+  login(): void {
+    console.log(this.formLogin.valid);
+    console.log(this.formLogin.value);
+
+
   }
+
 
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  formRegister: FormGroup = this.fb.group({
+    nombre: ['test1', [ Validators.required, Validators.minLength(3) ] ],
+    correo: ['test1@test.cl', [ Validators.required, Validators.email ] ],
+    contrasena: ['123456', [ Validators.required, Validators.minLength(6) ] ],
+  });
 
-  ngOnInit(): void {
+  constructor( private fb: FormBuilder) { }
+
+  register(): void {
+    console.log(this.formRegister.valid);
+    console.log(this.formRegister.value);
   }
 
 }
